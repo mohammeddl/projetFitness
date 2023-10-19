174f5974bf511b232d09e7a9530447d3d71857c5
mohammeddl <mohammed_daali2@um5.ac.ma>
2023-10-19 19:20:17 +0100

    add js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll('.faq-question');
+        questions.forEach(question => {
+            question.addEventListener('click', () => {
+                const answer = question.nextElementSibling;
+                if (answer.style.display === 'block') {
+                    answer.style.display = 'none';
+                } else {
+                    answer.style.display = 'block';
+                }
+            });
+        });
